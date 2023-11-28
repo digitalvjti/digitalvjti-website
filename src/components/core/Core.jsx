ccd8f344a90b3cd383bbd205a7f95cc5257fb452
Aaron Augustine <78950930+GravenHawkX@users.noreply.github.com>
2023-11-28 10:09:41 +0530

    Cards updated

--- src/components/core/Core.jsx
+++ src/components/core/Core.jsx
@@ -1,56 +1,276 @@
-import "./core.css";
 
+// import { CCard } from '@coreui/react'
+// import { CCardBody } from '@coreui/react'
+// import { CCardImage } from '@coreui/react'
+// import { CCardText } from '@coreui/react'
+// import { CButton } from '@coreui/react'
+// import { CCardTitle } from '@coreui/react'
+import linkedin_icon from '../Team/linkedin.png'
+
+import './core.css'
 const Core = () => {
-  return (
-    <div>
-      <h2 className="event-title">Event gallery</h2>
-
-      <div className="wrapper">
-        <div className="container">
-          <input type="radio" name="slide" id="c1" />
-          <label htmlFor="c1" className="card">
-            <div className="row">
-              <div className="icon">1</div>
-              <div className="description">
-                <h4>Name XYZ</h4>
-                <p>Position</p>
-              </div>
+    return ( 
+      
+      <div>
+      <h2  className="event-title">
+        Core Committee
+      </h2>
+      <h3  className="event-subtitle">
+          Committe Heads
+      </h3>
+      <div className="committee_heads">
+        <label htmlFor="c1" className="card">
+          <div className="row">
+            <div className="icon"><a href="https://www.linkedin.com/in/rishi-patil-64b23b227?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app" ><img src={linkedin_icon} alt="" /></a></div>
+            <div className="description">
+              <h4>Rishi Patil </h4>
+              <p>Committee Head</p>
             </div>
-          </label>
-          <input type="radio" name="slide" id="c2" />
-          <label htmlFor="c2" className="card">
-            <div className="row">
-              <div className="icon">2</div>
-              <div className="description">
-                <h4>Digital Technology</h4>
-                <p>Gets better every day - stay tuned</p>
-              </div>
+          </div>
+        </label>
+        <label htmlFor="c2" className="card">
+          <div className="row">
+          <div className="icon"><a href="http://www.linkedin.com/in/raghav-korde" ><img src={linkedin_icon} alt="" /></a></div>
+            <div className="description">
+              <h4>Raghav Korde</h4>
+              <p>Committee Head</p>
             </div>
-          </label>
-          <input type="radio" name="slide" id="c3" />
+          </div>
+        </label>
+        </div>
+        <h3  className="event-subtitle">
+          Treasurers
+        </h3>
+        <div className="treasurers">
           <label htmlFor="c3" className="card">
             <div className="row">
-              <div className="icon">3</div>
+            <div className="icon"><a href="https://www.linkedin.com/in/ira-sheth-731162282?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" ><img src={linkedin_icon} alt="" /></a></div>
               <div className="description">
-                <h4>Globalization</h4>
-                <p>Help people all over the world</p>
+                <h4>Ira Sheth</h4>
+                <p>Treasurer</p>
               </div>
             </div>
           </label>
-          <input type="radio" name="slide" id="c4" />
           <label htmlFor="c4" className="card">
             <div className="row">
-              <div className="icon">4</div>
+            <div className="icon"><a href="https://www.linkedin.com/in/moh-owais-sheikh-271660256?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BNXd4e0aITOiPUGcAS1Zg6Q%3D%3D" ><img src={linkedin_icon} alt="" /></a></div>
               <div className="description">
-                <h4>New technologies</h4>
-                <p>Space engineering becomes more and more advanced</p>
+                <h4>Mohd Owais Sheikh </h4>
+                <p>Treasurer</p>
               </div>
             </div>
           </label>
         </div>
-      </div>
-    </div>
-  );
-};
+        <h3  className="event-subtitle">
+          Social Media Heads
+        </h3>
+        <div className="social_heads">
+        <label htmlFor="c5" className="card">
+          <div className="row">
+          <div className="icon"><a href="https://www.linkedin.com/in/shashankdhurwey?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" className="linkedin_acc"><img src={linkedin_icon} alt="" /></a></div>
+            <div className="description">
+              <h4>Shashank Dhurwey</h4>
+              <p>Social Media Head</p>
+            </div>
+          </div>
+        </label>
+        <label htmlFor="c6" className="card">
+          <div className="row">
+          <div className="icon"><a href="https://www.linkedin.com/in/vedank-gaikwad-26053428b?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" className="linkedin_acc"><img src={linkedin_icon} alt="" /></a></div>
+            <div className="description">
+              <h4>Vedank Gaikwad</h4>
+              <p>Social Media Head</p>
+            </div>
+          </div>
+        </label>
+        </div>
+        <h3  className="event-subtitle">
+          Technical Heads
+        </h3>
+        <div className="tech_heads">
+        <label htmlFor="c7" className="card">
+          <div className="row">
+          <div className="icon"><a href="https://www.linkedin.com/in/tanaya-shelke-209701230?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" className="linkedin_acc"><img src={linkedin_icon} alt="" /></a></div>
+            <div className="description">
+              <h4>Tanaya Shelke</h4>
+              <p>Technical Head</p>
+            </div>
+          </div>
+        </label>
+        <label htmlFor="c8" className="card">
+          <div className="row">
+          <div className="icon"><a href="https://www.linkedin.com/in/aaron-augustine09/" className="linkedin_acc"><img src={linkedin_icon} alt="" /></a></div>
+            <div className="description">
+              <h4>Aaron Augustine</h4>
+              <p>Technical Head</p>
+            </div>
+          </div>
+        </label>
+        <label htmlFor="c9" className="card">
+          <div className="row">
+          <div className="icon"><a href="https://www.linkedin.com/in/aom-wankhede-b51281186/" className="linkedin_acc"><img src={linkedin_icon} alt="" /></a></div>
+            <div className="description">
+              <h4>Aom Wankhede</h4>
+              <p>Technical Head</p>
+            </div>
+          </div>
+        </label>
+        <label htmlFor="c22" className="card">
+          <div className="row">
+          <div className="icon"><a href="" className="linkedin_acc"><img src={linkedin_icon} alt="" /></a></div>
+            <div className="description">
+              <h4>Sailie Ghole</h4>
+              <p>Technical Head</p>
+            </div>
+          </div>
+        </label>
+        <label htmlFor="c10" className="card">
+          <div className="row">
+          <div className="icon"><a href="https://www.linkedin.com/in/keyur-doshi65/" className="linkedin_acc"><img src={linkedin_icon} alt="" /></a></div>
+            <div className="description">
+              <h4>Keyur Doshi</h4>
+              <p>Technical Head</p>
+            </div>
+          </div>
+        </label>
+        <label htmlFor="c11" className="card">
+          <div className="row">
+          <div className="icon"><a href="https://www.linkedin.com/in/akshay-potkhule-30a424229?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" className="linkedin_acc"><img src={linkedin_icon} alt="" /></a></div>
+            <div className="description">
+              <h4>Akshay Potkhule</h4>
+              <p>Technical Head</p>
+            </div>
+          </div>
+        </label>
+        <label htmlFor="c12" className="card">
+          <div className="row">
+          <div className="icon"><a href="https://www.linkedin.com/in/nikhil-soni-00b63522b?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" className="linkedin_acc"><img src={linkedin_icon} alt="" /></a></div>
+            <div className="description">
+              <h4>Nikhil Soni</h4>
+              <p>Technical Head</p>
+            </div>
+          </div>
+        </label>
+        <label htmlFor="c13" className="card">
+          <div className="row">
+          <div className="icon"><a href="https://www.linkedin.com/in/rushi-jani-4a71ab233" className="linkedin_acc"><img src={linkedin_icon} alt="" /></a></div>
+            <div className="description">
+              <h4>Rushi Jani</h4>
+              <p>Technical Head</p>
+            </div>
+          </div>
+        </label>
+        </div>
+        <h3 className="event-subtitle">
+          Operation Heads
+        </h3>
+        <div className="op_heads">
+        <label htmlFor="c14" className="card">
+          <div className="row">
+          <div className="icon"><a href="https://www.linkedin.com/in/akshada-pathak-605390252?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" className="linkedin_acc"><img src={linkedin_icon} alt="" /></a></div>
+            <div className="description">
+              <h4>Akshada Pathak</h4>
+              <p>Operations Head</p>
+            </div>
+          </div>
+        </label>
+        <label htmlFor="c15" className="card">
+          <div className="row">
+          <div className="icon"><a href="https://www.linkedin.com/in/aayush-deshpande-10b825256?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" className="linkedin_acc"><img src={linkedin_icon} alt="" /></a></div>
+            <div className="description">
+              <h4>Aayush Deshpande</h4>
+              <p>Operations Head</p>
+            </div>
+          </div>
+        </label>
+        </div>
+        <h3 className="event-subtitle">
+          Design Heads
+        </h3>
+        <div className="design_heads">
+        <label htmlFor="c16" className="card">
+          <div className="row">
+          <div className="icon"><a href="https://www.linkedin.com/in/amisha-sane623" className="linkedin_acc"><img src={linkedin_icon} alt="" /></a></div>
+            <div className="description">
+              <h4>Amisha Sane</h4>
+              <p>Design Head</p>
+            </div>
+          </div>
+        </label>
+        <label htmlFor="c17" className="card">
+          <div className="row">
+          <div className="icon"><a href="https://www.linkedin.com/in/moh-owais-sheikh-271660256?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BNXd4e0aITOiPUGcAS1Zg6Q%3D%3D" className="linkedin_acc"><img src={linkedin_icon} alt="" /></a></div>
+            <div className="description">
+              <h4>Mohd Owais Sheikh</h4>
+              <p>Design Head</p>
+            </div>
+          </div>
+        </label>
+        </div>
+        <h3 className="event-subtitle">
+          Marketing Head
+        </h3>
+        <div className="marketing">
+        <label htmlFor="c18" className="card">
+          <div className="row">
+          <div className="icon"><a href="https://www.linkedin.com/in/aashish-dubey-844154187?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" className="linkedin_acc"><img src={linkedin_icon} alt="" /></a></div>
+            <div className="description">
+              <h4>Aashish Dubey</h4>
+              <p>Marketing Head</p>
+            </div>
+          </div>
+        </label>
+        </div>
+        <h3 className="event-subtitle">
+          Content Writing
+        </h3>
+        <div className="content_writing">
+          <label htmlFor="c19" className="card">
+          <div className="row">
+          <div className="icon"><a href="https://www.linkedin.com/in/tanushri-warade-80a00222b?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" className="linkedin_acc"><img src={linkedin_icon} alt="" /></a></div>
+            <div className="description">
+              <h4>Tanshuri Warade</h4>
+              <p>Content Writing</p>
+            </div>
+          </div>
+        </label>
+        <label htmlFor="c20" className="card">
+          <div className="row">
+          <div className="icon"><a href="" className="linkedin_acc"><img src={linkedin_icon} alt="" /></a></div>
+            <div className="description">
+              <h4>Priyanka Kamble</h4>
+              <p>Content Writing</p>
+            </div>
+          </div>
+        </label>
+        <label htmlFor="c21" className="card">
+          <div className="row">
+          <div className="icon"><a href="https://www.linkedin.com/in/aryan-shah-ba33b7238/" className="linkedin_acc"><img src={linkedin_icon} alt="" /></a></div>
+            <div className="description">
+              <h4>Aryan Shah</h4>
+              <p>Content Writing</p>
+            </div>
+          </div>
+        </label>
+        </div>
+        <h3 className="event-subtitle">
+          Pr and Sponsor Head
+        </h3>
+        <div className="pr_and_spons">
+        <label htmlFor="c23" className="card">
+          <div className="row">
+          <div className="icon"><a href="https://www.linkedin.com/in/rushabh-shah-1778a6232?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" className="linkedin_acc"><img src={linkedin_icon} alt="" /></a></div>
+            <div className="description">
+              <h4>Rushabh Shah</h4>
+              <p>Pr and Sponsor Headg</p>
+            </div>
+          </div>
+        </label>
+        </div>
+        
 
+      </div>
+     );
+}
+ 
 export default Core;
