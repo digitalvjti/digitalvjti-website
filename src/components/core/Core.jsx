b43a1b757cb21ef94f95d86cafc5a0acf6ed74c3
Aaron Augustine <78950930+GravenHawkX@users.noreply.github.com>
2023-11-29 16:06:29 +0530

    card changes

--- src/components/core/Core.jsx
+++ src/components/core/Core.jsx
@@ -21,7 +21,7 @@ const Core = () => {
       <div className="committee_heads">
         <label htmlFor="c1" className="card">
           <div className="row">
-            <div className="icon"><a href="https://www.linkedin.com/in/rishi-patil-64b23b227?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app" ><img src={linkedin_icon} alt="" /></a></div>
+            <div className="icon"><a href="https://www.linkedin.com/in/rishi-patil-64b23b227/" ><img src={linkedin_icon} alt="" /></a></div>
             <div className="description">
               <h4>Rishi Patil </h4>
               <p>Committee Head</p>
@@ -44,7 +44,7 @@ const Core = () => {
         <div className="treasurers">
           <label htmlFor="c3" className="card">
             <div className="row">
-            <div className="icon"><a href="https://www.linkedin.com/in/ira-sheth-731162282?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" ><img src={linkedin_icon} alt="" /></a></div>
+            <div className="icon"><a href="https://www.linkedin.com/in/ira-sheth-731162282" ><img src={linkedin_icon} alt="" /></a></div>
               <div className="description">
                 <h4>Ira Sheth</h4>
                 <p>Treasurer</p>
@@ -53,9 +53,9 @@ const Core = () => {
           </label>
           <label htmlFor="c4" className="card">
             <div className="row">
-            <div className="icon"><a href="https://www.linkedin.com/in/moh-owais-sheikh-271660256?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BNXd4e0aITOiPUGcAS1Zg6Q%3D%3D" ><img src={linkedin_icon} alt="" /></a></div>
+            <div className="icon"><a href="https://www.linkedin.com/in/moh-owais-sheikh-271660256" ><img src={linkedin_icon} alt="" /></a></div>
               <div className="description">
-                <h4>Mohd Owais Sheikh </h4>
+                <h4>Mohd Owais Sheikh</h4>
                 <p>Treasurer</p>
               </div>
             </div>
@@ -67,7 +67,7 @@ const Core = () => {
         <div className="social_heads">
         <label htmlFor="c5" className="card">
           <div className="row">
-          <div className="icon"><a href="https://www.linkedin.com/in/shashankdhurwey?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" className="linkedin_acc"><img src={linkedin_icon} alt="" /></a></div>
+          <div className="icon"><a href="https://www.linkedin.com/in/shashankdhurwey" className="linkedin_acc"><img src={linkedin_icon} alt="" /></a></div>
             <div className="description">
               <h4>Shashank Dhurwey</h4>
               <p>Social Media Head</p>
@@ -76,7 +76,7 @@ const Core = () => {
         </label>
         <label htmlFor="c6" className="card">
           <div className="row">
-          <div className="icon"><a href="https://www.linkedin.com/in/vedank-gaikwad-26053428b?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" className="linkedin_acc"><img src={linkedin_icon} alt="" /></a></div>
+          <div className="icon"><a href="https://www.linkedin.com/in/vedank-gaikwad-26053428b" className="linkedin_acc"><img src={linkedin_icon} alt="" /></a></div>
             <div className="description">
               <h4>Vedank Gaikwad</h4>
               <p>Social Media Head</p>
@@ -90,7 +90,7 @@ const Core = () => {
         <div className="tech_heads">
         <label htmlFor="c7" className="card">
           <div className="row">
-          <div className="icon"><a href="https://www.linkedin.com/in/tanaya-shelke-209701230?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" className="linkedin_acc"><img src={linkedin_icon} alt="" /></a></div>
+          <div className="icon"><a href="https://www.linkedin.com/in/tanaya-shelke-209701230" className="linkedin_acc"><img src={linkedin_icon} alt="" /></a></div>
             <div className="description">
               <h4>Tanaya Shelke</h4>
               <p>Technical Head</p>
@@ -117,7 +117,7 @@ const Core = () => {
         </label>
         <label htmlFor="c22" className="card">
           <div className="row">
-          <div className="icon"><a href="" className="linkedin_acc"><img src={linkedin_icon} alt="" /></a></div>
+          <div className="icon"><a href="https://www.linkedin.com/in/sailie-ghole-b3737722a/" className="linkedin_acc"><img src={linkedin_icon} alt="" /></a></div>
             <div className="description">
               <h4>Sailie Ghole</h4>
               <p>Technical Head</p>
@@ -135,7 +135,7 @@ const Core = () => {
         </label>
         <label htmlFor="c11" className="card">
           <div className="row">
-          <div className="icon"><a href="https://www.linkedin.com/in/akshay-potkhule-30a424229?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" className="linkedin_acc"><img src={linkedin_icon} alt="" /></a></div>
+          <div className="icon"><a href="https://www.linkedin.com/in/akshay-potkhule-30a424229" className="linkedin_acc"><img src={linkedin_icon} alt="" /></a></div>
             <div className="description">
               <h4>Akshay Potkhule</h4>
               <p>Technical Head</p>
@@ -144,7 +144,7 @@ const Core = () => {
         </label>
         <label htmlFor="c12" className="card">
           <div className="row">
-          <div className="icon"><a href="https://www.linkedin.com/in/nikhil-soni-00b63522b?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" className="linkedin_acc"><img src={linkedin_icon} alt="" /></a></div>
+          <div className="icon"><a href="https://www.linkedin.com/in/nikhil-soni-00b63522b" className="linkedin_acc"><img src={linkedin_icon} alt="" /></a></div>
             <div className="description">
               <h4>Nikhil Soni</h4>
               <p>Technical Head</p>
@@ -167,7 +167,7 @@ const Core = () => {
         <div className="op_heads">
         <label htmlFor="c14" className="card">
           <div className="row">
-          <div className="icon"><a href="https://www.linkedin.com/in/akshada-pathak-605390252?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" className="linkedin_acc"><img src={linkedin_icon} alt="" /></a></div>
+          <div className="icon"><a href="https://www.linkedin.com/in/akshada-pathak-605390252" className="linkedin_acc"><img src={linkedin_icon} alt="" /></a></div>
             <div className="description">
               <h4>Akshada Pathak</h4>
               <p>Operations Head</p>
@@ -176,7 +176,7 @@ const Core = () => {
         </label>
         <label htmlFor="c15" className="card">
           <div className="row">
-          <div className="icon"><a href="https://www.linkedin.com/in/aayush-deshpande-10b825256?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" className="linkedin_acc"><img src={linkedin_icon} alt="" /></a></div>
+          <div className="icon"><a href="https://www.linkedin.com/in/aayush-deshpande-10b825256" className="linkedin_acc"><img src={linkedin_icon} alt="" /></a></div>
             <div className="description">
               <h4>Aayush Deshpande</h4>
               <p>Operations Head</p>
@@ -199,7 +199,7 @@ const Core = () => {
         </label>
         <label htmlFor="c17" className="card">
           <div className="row">
-          <div className="icon"><a href="https://www.linkedin.com/in/moh-owais-sheikh-271660256?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BNXd4e0aITOiPUGcAS1Zg6Q%3D%3D" className="linkedin_acc"><img src={linkedin_icon} alt="" /></a></div>
+          <div className="icon"><a href="https://www.linkedin.com/in/moh-owais-sheikh-271660256" className="linkedin_acc"><img src={linkedin_icon} alt="" /></a></div>
             <div className="description">
               <h4>Mohd Owais Sheikh</h4>
               <p>Design Head</p>
@@ -213,7 +213,7 @@ const Core = () => {
         <div className="marketing">
         <label htmlFor="c18" className="card">
           <div className="row">
-          <div className="icon"><a href="https://www.linkedin.com/in/aashish-dubey-844154187?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" className="linkedin_acc"><img src={linkedin_icon} alt="" /></a></div>
+          <div className="icon"><a href="https://www.linkedin.com/in/aashish-dubey-844154187" className="linkedin_acc"><img src={linkedin_icon} alt="" /></a></div>
             <div className="description">
               <h4>Aashish Dubey</h4>
               <p>Marketing Head</p>
@@ -227,9 +227,9 @@ const Core = () => {
         <div className="content_writing">
           <label htmlFor="c19" className="card">
           <div className="row">
-          <div className="icon"><a href="https://www.linkedin.com/in/tanushri-warade-80a00222b?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" className="linkedin_acc"><img src={linkedin_icon} alt="" /></a></div>
+          <div className="icon"><a href="https://www.linkedin.com/in/tanushri-warade-80a00222b" className="linkedin_acc"><img src={linkedin_icon} alt="" /></a></div>
             <div className="description">
-              <h4>Tanshuri Warade</h4>
+              <h4>Tanushri Warade</h4>
               <p>Content Writing</p>
             </div>
           </div>
@@ -259,10 +259,10 @@ const Core = () => {
         <div className="pr_and_spons">
         <label htmlFor="c23" className="card">
           <div className="row">
-          <div className="icon"><a href="https://www.linkedin.com/in/rushabh-shah-1778a6232?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" className="linkedin_acc"><img src={linkedin_icon} alt="" /></a></div>
+          <div className="icon"><a href="https://www.linkedin.com/in/rushabh-shah-1778a6232" className="linkedin_acc"><img src={linkedin_icon} alt="" /></a></div>
             <div className="description">
               <h4>Rushabh Shah</h4>
-              <p>Pr and Sponsor Headg</p>
+              <p>Pr and Sponsor Head</p>
             </div>
           </div>
         </label>
